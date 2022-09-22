46f007ba62abb60e96ba3f7dfdbe6e861e3216f6
Isana Kashiwai <isana.k@gmail.com>
2022-09-22 18:06:41 +0900

    modify build process & build minified version

--- rollup.config.js
+++ rollup.config.js
@@ -8,11 +8,28 @@ module.exports = [
       babel({
         exclude: "node_modules/**",
         babelHelpers: "bundled",
+      })
+    ],
+    output: {
+      file: 'dev/js/mdview.rc.js',
+      format: 'iife',
+    },
+    onwarn: (warning, defaultHandler) => {
+      if (warning.code === 'THIS_IS_UNDEFINED') return;
+      defaultHandler(warning)
+    }
+  },
+  {
+    input: 'dev/js/mdview.js',
+    plugins: [
+      babel({
+        exclude: "node_modules/**",
+        babelHelpers: "bundled",
       }),
       terser()
     ],
     output: {
-      file: 'dev/js/mdview.rc.js',
+      file: 'dev/js/mdview.min.rc.js',
       format: 'iife',
     },
     onwarn: (warning, defaultHandler) => {
